328911be82f3e8f4afa879424f5bc7512703f9f4
b0ses <b0ses@users.noreply.github.com>
2017-09-09 23:28:12 -0400

    Initial Hello World

---
+++ hello/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+# -*- coding: utf-8 -*-
+
+from django.views.generic import TemplateView
+class HomeView(TemplateView):
+    template_name = 'index.html'
